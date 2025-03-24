d14fe907a5e27682293c1b3651173c8be1f6c391
WizzOFF <maher_off@proton.me>
2025-03-24 03:02:11 +0100

    first_program.cpp: cout exceptions

--- first_program.cpp
+++ first_program.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 
 using namespace std;
-
+/*
 int main()
 {
     cout << "Hello world!" << endl;
     return 0;
+}
+*/
+
+int main()
+{
+  cout << "Je fais des tests pour apprendre le C++ !" << endl;
+  cout << "\"" << endl;
+  cout << "\\" << endl;
+  return 0;
 }
